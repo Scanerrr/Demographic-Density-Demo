e5883e06800ccc47e52f497216e906a6e3e37d93
bohdan.nekhai <bohdan.nekhai@agilie.com>
2020-01-08 14:37:09 +0200

    Updates

--- precache-manifest.78848480f55d42bbf5e35f7b64b139a1.js
+++ precache-manifest.78848480f55d42bbf5e35f7b64b139a1.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1c5fc7d1ca3892e00d993ebf9e1cb858",
+    "revision": "e304ef1061ea195a6fee3505dfe1aad8",
     "url": "/Demographic-Density-Demo/index.html"
   },
   {
-    "revision": "7759d48d127640b0569b",
+    "revision": "f50088502143c3aae191",
     "url": "/Demographic-Density-Demo/static/css/main.53809aff.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Demographic-Density-Demo/static/js/2.95b73df9.chunk.js.LICENSE"
   },
   {
-    "revision": "7759d48d127640b0569b",
-    "url": "/Demographic-Density-Demo/static/js/main.46bc19a9.chunk.js"
+    "revision": "f50088502143c3aae191",
+    "url": "/Demographic-Density-Demo/static/js/main.876e4312.chunk.js"
   },
   {
     "revision": "19b71f389c2ac528423e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Demographic-Density-Demo/precache-manifest.ef9f0a52428588aed4b9bd5e44fadfd0.js"
+  "/Demographic-Density-Demo/precache-manifest.78848480f55d42bbf5e35f7b64b139a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Demographic-Density-Demo/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
